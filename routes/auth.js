c5a750a117bdb1d383369ea4966f5a5449456917
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-21 14:40:18 -0400

    small clean up to syntax

--- routes/auth.js
+++ routes/auth.js
@@ -6,20 +6,24 @@ var db = require('../db');
 
 var router = express.Router();
 
-// added passport, and crypto
+// added: Strategies are responsible for authenticating requests
 passport.use(new LocalStrategy(function verify(username, password, cb) {
-  db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
-
-    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+  db.get(
+    'SELECT * FROM users WHERE username = ?',
+    [ username ],
+    function(err, row) {
       if (err) { return cb(err); }
-      if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      return cb(null, row);
-    });
-  });
+      if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+      crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
+        if (err) { return cb(err); }
+        if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+          return cb(null, false, { message: 'Incorrect username or password.' });
+        }
+        return cb(null, row);
+      });
+    }
+  );
 }));
 
 // added: to configure passpoer to persist user information in the login session
